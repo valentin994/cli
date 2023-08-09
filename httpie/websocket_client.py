b3246b40f13ea81509b6049efd9d9c9613b1dd4e
Valentin Vareskic <valentin.vareskic@gmail.com>
2023-08-09 15:55:52 +0200

    added timed function to keep the connection alive

--- httpie/websocket_client.py
+++ httpie/websocket_client.py
@@ -3,20 +3,21 @@ from websockets.client import ClientProtocol
 from websockets.sync.client import ClientConnection
 from websockets.uri import parse_uri
 from websockets.exceptions import InvalidURI
+import threading
 
 
-def create_connection(uri, timeout: float = 0.1) -> None:
+def create_connection(uri, timeout: float = 0.1, message: str = None) -> None:
     try:
         uri = parse_uri(uri)
     except InvalidURI:
-        #TODO implement error handling
+        # TODO implement error handling
         print("invalid")
         pass
     protocol = ClientProtocol(uri)
     sock = socket.create_connection((uri.host, uri.port))
     connection = ClientConnection(sock, protocol, close_timeout=timeout)
     connection.handshake()
-    send_message(connection, "hello")
+    send_message(connection, message)
 
 
 def send_message(connection: ClientConnection, message: str) -> None:
@@ -25,6 +26,7 @@ def send_message(connection: ClientConnection, message: str) -> None:
         connection.close(code=1000, reason="Sent the message")
     data = ""
     while data != "exit()":
+        threading.Timer(5, connection.send)
         data = input()
         connection.send(data)
     connection.close(code=1000, reason="No more messages to be sent")
